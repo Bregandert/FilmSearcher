183fc7fb65b07859b8a55873c6f261f0fbb02670
Bregandert <bregandert84@gmail.com>
2023-07-23 13:33:44 +0300

    Screen layout

--- app/src/main/java/com/bregandert/filmsearcher/MainActivity.kt
+++ app/src/main/java/com/bregandert/filmsearcher/MainActivity.kt
@@ -14,33 +14,9 @@ import com.bregandert.filmsearcher.ui.theme.FilmSearcherTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        setContent {
-            FilmSearcherTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FilmSearcherTheme {
-        Greeting("Android")
-    }
-}
